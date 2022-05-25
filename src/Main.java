1d6d0f41414be8e7b07328d47d749493b6717652
Nikita <athlon64x2600@gmail.com>
2022-05-25 22:34:48 +0200

    Задание 3 високосным является каждый четвертый год, но не является каждый сотый. Также високосным является каждый четырехсотый год.

--- src/Main.java
+++ src/Main.java
@@ -21,5 +21,15 @@ public class Main {
         if (clientDeviceYear < 2015 && clientOS == 0) {
             System.out.println("Установите облегченную версию приложения для iOS по ссылке");
         }
+
+        // Задание 3
+
+        int year = 2204;
+
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+            System.out.println(year + " год является високосным");
+        } else {
+            System.out.println(year + " год не является високосным");
+        }
     }
 }
