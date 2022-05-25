1178fa3ce574ca6c514d7dd8e3592dda465bf509
Nikita <athlon64x2600@gmail.com>
2022-05-25 22:24:18 +0200

    Задание 2 Ваша задача: написать программу, которая выдает соответствующее сообщение клиенту при наличии двух условий — операционной системы телефона (iOS или Android) и года производства.

--- src/Main.java
+++ src/Main.java
@@ -10,5 +10,16 @@ public class Main {
         } else {
             System.out.println("Установите версию приложения для iOS по ссылке");
         }
+
+        // Задание 2
+
+        int clientDeviceYear = 2012;
+
+        if (clientDeviceYear < 2015 && clientOS == 1) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        }
+        if (clientDeviceYear < 2015 && clientOS == 0) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        }
     }
 }
